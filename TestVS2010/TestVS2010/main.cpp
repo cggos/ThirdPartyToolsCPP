172923718efa52750eeeaacae3ff1aa569964b17
GaoHongchen <1656738573@qq.com>
2017-03-24 09:29:04 +0800

    利用gtest对MathFuncs::GCD进行单元测试

--- TestVS2010/TestVS2010/main.cpp
+++ TestVS2010/TestVS2010/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <gtest/gtest.h>
 
-using namespace std;
+#include "..\..\Algorithms\MathFuncs.h"
 
-int main()
+TEST(MathFuncs, GCD)
 {
+	EXPECT_EQ(-2, MathFuncs::GCD(-4, -10));
+	EXPECT_EQ(6, MathFuncs::GCD(30, 18));
+}
+
+int main(int argc, char* argv[])
+{
+	//testing::GTEST_FLAG(output) = "xml:";
+	testing::InitGoogleTest(&argc, argv);
+	int ret = RUN_ALL_TESTS();
+
 	system("pause");
 	return 0;
 }
