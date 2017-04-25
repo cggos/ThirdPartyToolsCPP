daef15b6a207b742a3124275fb1186c19930f6ae
GaoHongchen <1656738573@qq.com>
2017-04-25 13:06:07 +0800

    屏蔽掉其中的GTest相关部分

--- TestVS2010/TestVS2010/main.cpp
+++ TestVS2010/TestVS2010/main.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
-#include <gtest/gtest.h>
 
-#include "..\..\Algorithms\MathFuncs.h"
-
-TEST(MathFuncs, GCD)
-{
-	EXPECT_EQ(-2, MathFuncs::GCD(-4, -10));
-	EXPECT_EQ(6, MathFuncs::GCD(30, 18));
-}
+//#include <gtest/gtest.h>
+//#pragma comment(lib,"..\..\googletest\googletest\msvc\gtest\Debug\gtestd.lib")
+//TEST(MathFuncs, GCD)
+//{
+//	EXPECT_EQ(-2, MathFuncs::GCD(-4, -10));
+//	EXPECT_EQ(6, MathFuncs::GCD(30, 18));
+//}
 
 int main(int argc, char* argv[])
 {
-	//testing::GTEST_FLAG(output) = "xml:";
-	testing::InitGoogleTest(&argc, argv);
-	int ret = RUN_ALL_TESTS();
+	////testing::GTEST_FLAG(output) = "xml:";
+	//testing::InitGoogleTest(&argc, argv);
+	//int ret = RUN_ALL_TESTS();
 
 	system("pause");
 	return 0;
